c44623e2bed6cde5dde7c43646b38736d5462520
Lucca Eiki <84162201+e1k1to@users.noreply.github.com>
2021-10-01 19:01:11 -0300

    Tradutor de emoji para python

---
+++ tradutor.py
@@ -0,0 +1,63 @@
+from operator import itemgetter as ig
+import sys
+emojiAliasses = {}
+def aliasses():
+    global emojiAliasses
+    emojiAliasses= {
+    '👋':'Hello',
+    '🌎':'World',
+    '🖨':'print',
+    '🌜':'(',
+    '🌛':')',
+    '❕':'!',
+    '❗':'!',
+    '📎':'"',
+    '🖇':"'",
+    '🕳':' ',
+    '👙':'strip',
+    '👍':'True',
+    '👎':'False',
+    '🔁':'for',
+    '🔂':'while',
+    '📑':'  ',
+    '📄':'"""',
+    '📝':'#',
+    '👐':'range',
+    '➡':'in',
+    '↩':'return',
+    '🚩':'raise',
+    '⛔':'break',
+    '🤝':'and',
+    '❌':'not',
+    '🙌':'or',
+    '⏪':'=',
+    }
+
+def emojiToPy(emojiCode):
+    emojiList = tuple(emojiCode)
+    codestart = False
+    code = ''
+    for emoji in emojiList:
+        if emoji in emojiAliasses:
+            code += ig(emoji)(emojiAliasses)
+        else:
+            code+= emoji
+    exec(code)
+
+def usage():
+    print('''py-emoji-lang usage:\n\npython3 compiler.py {file.mongus}''')
+
+def main():
+    aliasses()
+    if len(sys.argv) == 1:
+        print('Incorrect usage.')
+        usage()
+        sys.exit()
+    else:
+        file = sys.argv[1]
+        emojiCode = ''
+        with open(file,'r') as ecode:
+            emojiCode += str(ecode.read())
+        emojiToPy(emojiCode)
+
+main()
